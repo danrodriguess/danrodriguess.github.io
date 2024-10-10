5b1932797d0b8cf1bb3c26252ef41851c2f45f8e
danillorodrigues <danillo@voxtecnologia.com.br>
2024-10-10 11:47:42 -0300

    ajustes

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ function parseDate(dateString) {
     return dateString;
 }
 
+
 function copyToClipboard(text) {
     const textarea = document.createElement('textarea');
     textarea.value = text;
